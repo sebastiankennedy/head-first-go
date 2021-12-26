50052da55974a7b9d4cbdd0790c059212d01b75f
sebastiankennedy <sebastiankennedy@foxmail.com>
2021-12-26 19:31:24 +0800

    Added ch1.

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
-import "github.com/sebastiankennedy/head-first-go/ch2"
+import (
+	"github.com/sebastiankennedy/head-first-go/ch1"
+	"github.com/sebastiankennedy/head-first-go/ch2"
+)
 
 func main() {
+	ch1.HelloWorld()
+	ch1.Conversisons()
+
 	ch2.Guess()
 }
