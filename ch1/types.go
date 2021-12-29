888b0b3b830a4fbd37311afddfe6f2980bf9ef68
sebastiankennedy <sebastiankennedy@foxmail.com>
2021-12-29 12:27:48 +0800

    Added ch1.

---
+++ ch1/types.go
@@ -0,0 +1,13 @@
+package ch1
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func Types() {
+	fmt.Println(reflect.TypeOf(42))
+	fmt.Println(reflect.TypeOf(3.1415))
+	fmt.Println(reflect.TypeOf(true))
+	fmt.Println(reflect.TypeOf("Hello, Go!"))
+}
